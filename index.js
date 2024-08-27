28b7643c8d5a91799c130782ad7b25e9f5c8bb22
Arnau Ros <arnauros22@gmail.com>
2024-08-27 17:25:25 +0200

    push

--- index.js
+++ index.js
@@ -12,27 +12,30 @@ console.log("loaded");
 // Add an event listener to the submit button
 submitTask.addEventListener("click", function (event) {
   event.preventDefault();
+  if (inputTask.value !== "") {
+    // Create a new div to hold the task elements
+    const newTaskDiv = document.createElement("div");
+    console.log("container created");
 
-  // Create a new div to hold the task elements
-  const newTaskDiv = document.createElement("div");
-  console.log("container created");
+    // Clone the templates
+    const doneButton = doneButtonTemplate.cloneNode(true);
+    const textItem = textItemTemplate.cloneNode(true);
+    const deleteTask = deleteTaskTemplate.cloneNode(true);
 
-  // Clone the templates
-  const doneButton = doneButtonTemplate.cloneNode(true);
-  const textItem = textItemTemplate.cloneNode(true);
-  const deleteTask = deleteTaskTemplate.cloneNode(true);
+    // Set the text content of the cloned textItem to the input value
+    textItem.textContent = inputTask.value;
 
-  // Set the text content of the cloned textItem to the input value
-  textItem.textContent = inputTask.value;
+    // Append the cloned elements to the new task div
+    newTaskDiv.appendChild(doneButton);
+    newTaskDiv.appendChild(textItem);
+    newTaskDiv.appendChild(deleteTask);
 
-  // Append the cloned elements to the new task div
-  newTaskDiv.appendChild(doneButton);
-  newTaskDiv.appendChild(textItem);
-  newTaskDiv.appendChild(deleteTask);
+    // Append the new task div to the containerTasks div
+    containerTasks.appendChild(newTaskDiv);
 
-  // Append the new task div to the containerTasks div
-  containerTasks.appendChild(newTaskDiv);
-
-  // Clear the input field after adding the task
-  inputTask.value = "";
+    // Clear the input field after adding the task
+    inputTask.value = "";
+  } else {
+    alert("Please enter a task");
+  }
 });
