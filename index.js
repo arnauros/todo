9c48fd9688d4d523e7f1bfcfab34d49a06fbd588
Arnau Ros <arnauros22@gmail.com>
2024-08-27 16:50:11 +0200

    push

--- index.js
+++ index.js
@@ -8,12 +8,10 @@ const textItem = document.getElementById("textItem");
 const deleteTask = document.getElementById("deleteTask");
 
 //add an event listener to click the btn
-document
-  .querySelector("#submitTask")
-  .addEventListener("click", function (event) {
-    event.preventDefault();
+submitTask.addEventListener("click", function (event) {
+  event.preventDefault();
 
-    //add a new div
-    document.createElement("div");
-    console.log("container created");
-  });
+  //add a new div
+  document.createElement("div");
+  console.log("container created");
+});
