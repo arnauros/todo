1bba5145f9a27a384619d1c666802d53e3ae64b5
Arnau Ros <arnauros22@gmail.com>
2024-08-27 16:34:11 +0200

    push

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 "use strict";
+
+const submitTask = document.getElementById("submitTask");
+const inputTask = document.getElementById("inputTask");
+const containerTasks = document.getElementById("containerTasks");
+const doneButton = document.getElementById("doneButton");
+const textItem = document.getElementById("textItem");
+const deleteTask = document.getElementById("deleteTask");
+
+//add an event listener to click the btn
+document.querySelector = submitTask.addEventListener("click", function () {
+  //add a new div
+  const container = document.createElement("containerTasks");
+  console.log("container created");
+});
