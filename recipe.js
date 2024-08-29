e989028ba648aea1374d1de071e94dcc74afc7ef
Arnau Ros <arnauros22@gmail.com>
2024-08-29 10:26:38 +0200

    push

--- recipe.js
+++ recipe.js
@@ -1,1 +1,8 @@
 "use strict";
+
+const recipesWrapper = document.querySelector("recipesWrapper");
+const findRecipeButton = document.querySelector("findRecipeButton");
+
+findRecipeButton.addEventListener("click", (e) => {
+  e.preventDefault();
+});
