66ff1f99bcdd44bdb8cdae07b3d2a8449a6243c0
Arnau Ros <arnauros22@gmail.com>
2024-08-29 13:32:12 +0200

    push

--- recipe.js
+++ recipe.js
@@ -11,23 +11,33 @@ console.log("recipe.js is connected");
 const recipesWrapper = document.querySelector("#recipesWrapper");
 const findRecipeButton = document.querySelector("#findRecipeButton");
 const inputRecipe = document.querySelector("#inputRecipe");
-const recipe = inputRecipe.value;
 
 //event listeners
 findRecipeButton.addEventListener("click", (e) => {
   e.preventDefault();
+  const recipe = inputRecipe.value;
   console.log("btn clcike");
   console.log(recipe);
+  findRecipes(recipe);
 });
 
-const findRecipes = function () {
+const findRecipes = function (recipe) {
   const apiUrl = `https://api.edamam.com/api/recipes/v2?type=public&q=${recipe}&app_id=${appId}&app_key=${appKey}`;
-};
 
-fetch(apiUrl).then((response) => {
-  if (response.ok) {
-    console.log("response is ok");
-  } else {
-    console.log("response is not ok");
-  }
-});
+  fetch(apiUrl)
+    .then((response) => {
+      if (response.ok) {
+        console.log("response is ok");
+        return response.json();
+      } else {
+        console.log("response is not ok");
+        throw new Error("Error not ok");
+      }
+    });
+    .then((data) => {
+      console.log(data);
+    }
+    .catch((error) => {
+      console.log(error);
+    }))
+};
