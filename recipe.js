2622f802c0c0eebc93ce9b69b440548b97487164
Arnau Ros <arnauros22@gmail.com>
2024-08-29 10:41:26 +0200

    push

--- recipe.js
+++ recipe.js
@@ -4,8 +4,8 @@
 const apiKey = "b3239c2d04d2a67ff2cc7a4c6ea25daf";
 
 //inputs
-const recipesWrapper = document.querySelector("recipesWrapper");
-const findRecipeButton = document.querySelector("findRecipeButton");
+const recipesWrapper = document.querySelector("#recipesWrapper");
+const findRecipeButton = document.querySelector("#findRecipeButton");
 const inputRecipe = document.querySelector("#inputRecipe").value;
 
 console.log(inputRecipe);
